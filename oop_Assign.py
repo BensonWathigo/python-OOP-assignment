e1c8fd9c8ef71cc58a973070a83e4f35c1e09dbb
Benson Wathigo <bensonwathigo21@gmail.com>
2024-04-08 11:40:56 +0300

    first commit

---
+++ oop_Assign.py
@@ -0,0 +1,15 @@
+class person:
+    def __init__(self, name, age , gender):
+        self.name = 'Jonte'
+        self.age = 23
+        self.gender = 'Male'
+
+    def introduction (self):
+        print (f'Kwa majina ni ${self.name}, miaka ni ${self.age} and I am ${self.gender}.')
+
+Mkurugenzi = person ('Johnte',23,'Male')
+
+#method
+Mkurugenzi.introduction()
+
+
